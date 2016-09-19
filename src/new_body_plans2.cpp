e6f3a351dd0f58ebb173063602eef598e4d32dd5
Heather Goldsby <heather.goldsby@gmail.com>
2016-09-19 12:28:33 -0400

    added movie ko to nbp

--- src/new_body_plans2.cpp
+++ src/new_body_plans2.cpp
@@ -156,6 +156,8 @@ public:
         add_tool<analysis::dominant_reduced_graph>(this);
         
         add_tool<ealib::analysis::markov_movie>(this);
+        add_tool<ealib::analysis::markov_movie_ko>(this);
+
         add_tool<ealib::analysis::recalc_fit>(this);
         
     }
