fe523703bd6d9f81e8e9af114bec8e47ddd5bbb4
Heather Goldsby <heather.goldsby@gmail.com>
2017-03-06 12:48:01 -0500

    big world

---
+++ src/big_world.cpp
@@ -0,0 +1,212 @@
+
+
+#include <boost/accumulators/accumulators.hpp>
+#include <boost/accumulators/statistics/stats.hpp>
+#include <boost/accumulators/statistics/mean.hpp>
+#include <boost/accumulators/statistics/max.hpp>
+
+#include <ea/mkv/markov_network_evolution.h>
+#include <ea/generational_models/moran_process.h>
+#include <ea/fitness_function.h>
+#include <ea/cmdline_interface.h>
+#include <ea/datafiles/fitness.h>
+#include "markov_movie.h"
+#include "hourglass.h"
+#include "new_body_plans2.h"
+
+using namespace std;
+using namespace boost::accumulators;
+using namespace ealib;
+using namespace mkv;
+
+
+
+
+
+/*! Sample fitness function for Markov networks.
+ */
+struct big_world : fitness_function<unary_fitness<double>, constantS, stochasticS> {
+    template <typename Individual, typename RNG, typename EA>
+    double operator()(Individual& ind, RNG& rng, EA& ea) {
+        // fixed LARGE grid size
+        int max_x = 100;
+        int max_y = 100;
+        int grid_size = max_x * max_y;
+        vector<int> agent_pos (grid_size, -1);
+        vector<int> exec_order (grid_size);
+        double f=0.0;
+        
+        
+        // get the "prototype" phenotype (markov network):
+        typename EA::phenotype_type &N = ealib::phenotype(ind, ea);
+        vector<typename EA::phenotype_type> as; //
+        
+        
+        int offset = get<START_POS>(ea,0);
+        for (int q=0; q<get<NUM_START_AGENTS>(ea,1); q++) {
+            as.push_back(N); //grid_size, N); // my agents or networks
+            int pos = q + offset;
+            if (pos >=  grid_size) {
+                pos -= grid_size;
+            }
+            agent_pos[pos] = q;
+            as[q].reset(rng.seed());
+        }
+        
+        for (int i=0; i<grid_size; ++i) {
+            exec_order[i] = i;
+        }
+        
+        
+        std::vector< std::vector<int> > cell_color(grid_size, std::vector<int>(2, 0));
+        update_big_world_N(get<WORLD_UPDATES>(ea,10), 0, agent_pos, exec_order, as, cell_color, ind, rng, ea);
+        
+        /* translate agent positions... */
+        int upper_left = -1; // upper left individual pops up first
+        
+        for (int i =0; i<agent_pos.size(); i++) {
+            int p = agent_pos[i];
+            if (p != -1) {
+                upper_left = i;
+                break;
+            }
+        }
+
+        // max digital tissue size. Must be much smaller than the world size (100x100)
+
+        int ul_y = floor(upper_left / max_x);
+        int ul_x = upper_left % max_x;
+        int dt_max_x = get<X_SIZE>(ea,10);
+        int dt_max_y = get<Y_SIZE>(ea,10);
+        int dt_grid = dt_max_x * dt_max_y;
+        
+        vector<int> agent_pos_small (dt_grid, -1);
+        
+
+        int count = 0;
+        for (int j=0; j<dt_max_y; j++){
+            for (int i=0; i<dt_max_x; i++) {
+                int xy = upper_left + i + (j*max_y);
+                agent_pos_small[count] = agent_pos[xy];
+                count++;
+            }
+        }
+        
+        
+        
+        
+        int fit_func = get<BODYPLAN>(ea,0) ;
+        switch(fit_func) {
+            case 2:
+                f = body_plan_a(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 3:
+                f = body_plan_b(dt_grid,dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 4:
+                f = body_plan_c(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 5:
+                f = body_plan_d(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 6:
+                f = body_plan_e(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 7:
+                f = body_plan_f(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 8:
+                f = body_plan_g(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 9:
+                f = body_plan_h(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 10:
+                f = body_plan_i(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 100:
+                f = body_plan_solid(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+            case 101:
+                f = body_plan_drift(dt_grid, dt_max_x, dt_max_y, agent_pos_small, as, ea);
+                break;
+                
+        }
+        
+        if (f == 0) {
+            f = 1;
+        }
+        return (f);
+        
+        
+        
+        
+    }
+};
+
+// Evolutionary algorithm definition.
+typedef markov_network_evolution
+< big_world
+, recombination::asexual
+, generational_models::moran_process< >
+> ea_type;
+
+/*! Define the EA's command-line interface.
+ */
+template <typename EA>
+class cli : public cmdline_interface<EA> {
+public:
+    virtual void gather_options() {
+        add_mkv_options(this);
+        
+        add_option<POPULATION_SIZE>(this);
+        add_option<MORAN_REPLACEMENT_RATE_P>(this);
+        add_option<RUN_UPDATES>(this);
+        add_option<RUN_EPOCHS>(this);
+        add_option<RNG_SEED>(this);
+        add_option<RECORDING_PERIOD>(this);
+        
+        add_option<X_SIZE>(this);
+        add_option<Y_SIZE>(this);
+        add_option<BRAIN_UPDATES>(this);
+        add_option<WORLD_UPDATES>(this);
+        add_option<INPUT_BIT_ERROR_PROB>(this);
+        add_option<HIDDEN_BIT_ERROR_PROB>(this);
+        add_option<OUTPUT_BIT_ERROR_PROB>(this);
+        
+        add_option<CAPABILITIES_OFF>(this);
+        add_option<AGENT_DEATH_PROB>(this);
+        add_option<NUM_START_AGENTS>(this);
+        add_option<BODYPLAN>(this);
+        add_option<APOP_THRESH>(this);
+        add_option<START_POS>(this);
+        add_option<RAND_ORDER>(this);
+        add_option<NO_REP_PERIOD>(this);
+        add_option<GATE_EPSILON>(this);
+        
+        
+        
+    }
+    
+    
+    virtual void gather_tools() {
+        add_tool<analysis::dominant_genetic_graph>(this);
+        add_tool<analysis::dominant_causal_graph>(this);
+        add_tool<analysis::dominant_reduced_graph>(this);
+        
+        add_tool<ealib::analysis::markov_movie>(this);
+        add_tool<ealib::analysis::markov_movie_ko>(this);
+        
+        add_tool<ealib::analysis::recalc_fit>(this);
+        add_tool<ealib::analysis::ko>(this);
+        
+    }
+    
+    virtual void gather_events(EA& ea) {
+        add_event<datafiles::fitness_dat>(ea);
+    };
+};
+LIBEA_CMDLINE_INSTANCE(ea_type, cli);
+
+
+
--- src/hourglass.h
+++ src/hourglass.h
@@ -1164,4 +1164,366 @@ void update_world_stigmergic_communication_apop_N(int n, std::vector<int>& agent
 
 
 
+// Run the world... no coordinates; stigmergic and communication
+// this world is BIG - way bigger than the max multicell size
+
+template <typename Individual, typename RNG, typename EA>
+void update_big_world_N(int n, int overall_time, std::vector<int>& agent_pos, std::vector<int>& exec_order, std::vector<typename EA::phenotype_type>& as, std::vector< std::vector<int> >& cell_color, Individual& ind, RNG& rng, EA& ea) {
+    
+    // max digital tissue size. Must be much smaller than the world size (100x100)
+    int dt_max_x = get<X_SIZE>(ea,10);
+    int dt_max_y = get<Y_SIZE>(ea,10);
+
+    
+    // max grid size
+    int max_x = 100;
+    int max_y = 100;
+    int max_size = max_x * max_y;
+    
+    
+    int min_agent_x = 101;
+    int max_agent_x = -1;
+    int min_agent_y = 101;
+    int max_agent_y = -1;
+    
+
+    for (int i =0; i<agent_pos.size(); i++) {
+        int p = agent_pos[i];
+        if (p != -1) {
+            int agent_y = floor(i / max_x);
+            int agent_x = i % max_x;
+            
+            if (agent_x < min_agent_x) { min_agent_x = agent_x; }
+            if (agent_x > max_agent_x) { max_agent_x = agent_x; }
+            if (agent_y < min_agent_y) { min_agent_y = agent_y; }
+            if (agent_y > min_agent_y) { max_agent_y = agent_y; }
+        }
+    }
+    
+    int dist_x = max_agent_x - min_agent_x;
+    int dist_y = max_agent_y - min_agent_y;
+    
+    
+    typename EA::phenotype_type &N = ealib::phenotype(ind, ea);
+    
+    // World update... this is where growth may occur.
+    // Run agents for X updates
+    
+    
+    // Inputs:
+    // (0) north color, (1) north color,
+    // (2) east color, (3) east color,
+    // (4) south color, (5) south color,
+    // (6) west color, (7) west color
+    // (8) origin (0,0)
+    // (9) edge
+    // (10) cell color, (11) cell color, (12) cell color
+    // (13 - 15) north message
+    // (16 - 18) east message
+    // (19 - 21) south message
+    // (22 - 24) west message
+    
+    
+    // Outputs:
+    // (0) color, (1) color
+    // (2) direction for repro, (3) direction for repro
+    // (4) move
+    // (5) reproduce
+    // (6) mark
+    // (7) mark color (8) mark color (9) mark color
+    // (10 - 12) out message
+    
+    
+    // for this grid, 0,0 is upper left.
+    // for this grid, 0,0 is upper left.
+    int brain_updates = get<BRAIN_UPDATES>(ea,10);
+    const std::string& capabilities_off = get<CAPABILITIES_OFF>(ea, "");
+    
+    bool origin = (!boost::algorithm::icontains(capabilities_off, "origin"));
+    bool migrate = (!boost::algorithm::icontains(capabilities_off, "migrate"));
+    bool reproduce = (!boost::algorithm::icontains(capabilities_off, "reproduce"));
+    bool stigmergic = (!boost::algorithm::icontains(capabilities_off, "stigmergic"));
+    bool edge = (!boost::algorithm::icontains(capabilities_off, "edge"));
+    bool communication = (!boost::algorithm::icontains(capabilities_off, "communication"));
+    bool sense = (!boost::algorithm::icontains(capabilities_off, "neighbor"));
+    
+    
+    int total_time = overall_time;
+    // agent_pos, as, exec_order
+    for(int t=0;t<n;t++){
+        
+        // Must randomize agent execution order...
+        if (get<RAND_ORDER>(ea,0) ==1) {
+            std::random_shuffle ( exec_order.begin(), exec_order.end(), ea.rng() );
+        }
+        
+        // Brain update... there should be about 4-8 brain updates per agent per world update
+        for (int j=0; j<exec_order.size(); ++j) {
+            // get x,y coord of agent
+            int xy = exec_order[j];
+            // where we can find the agent itself
+            int p = agent_pos[exec_order[j]];
+            
+            // no agent exists
+            if (p == -1) {
+                continue;
+            }
+            
+
+            // set the input states...
+            int agent_y = floor(xy / max_x);
+            int agent_x = xy % max_x;
+            int north = (agent_y - 1) * max_x + agent_x;
+            int east = agent_y * max_x + agent_x + 1;
+            int south = (agent_y + 1) * max_x + agent_x;
+            int west = agent_y * max_x + agent_x - 1;
+            
+            
+            
+            // migrate
+            if (migrate && as[p].output(4)) {
+                
+                if ((as[p].output(2) == 0) && (as[p].output(3)== 0) && (agent_y > 0)) { // 00 north
+                    if ((agent_pos[north] == -1) && (dist_y < dt_max_y)) {
+                        agent_pos[north] = p; // move agent
+                        agent_pos[xy] = -1;
+                        xy = north;
+                        min_agent_y = agent_y - 1;
+                        dist_y++;
+                    }
+                } else if ((as[p].output(2) == 0) && (as[p].output(3)== 1) && (agent_x < (max_x - 1))) {  // 01 east
+                    if ((agent_pos[east] == -1) && (dist_x < dt_max_x)) {
+                        agent_pos[east] = p; // move agent
+                        agent_pos[xy] = -1;
+                        xy = east;
+                        max_agent_x = agent_x + 1;
+                        dist_x++;
+                    }
+                } else if ((as[p].output(2) == 1) && (as[p].output(3)== 1) && (agent_y < (max_y - 1))) { // 11 south
+                    if ((agent_pos[south] == -1) && (dist_y < dt_max_y)){
+                        agent_pos[south] = p; // move agent
+                        agent_pos[xy] = -1;
+                        xy = south;
+                        max_agent_y = agent_y + 1;
+                        dist_y++;
+                    }
+                } else if ((as[p].output(2) == 1) && (as[p].output(3)== 0) && (agent_x > 0)) { // 10 west
+                    if ((agent_pos[west] == -1) && (dist_x < dt_max_x)){
+                        agent_pos[west] = p; // move agent
+                        agent_pos[xy] = -1;
+                        xy = west;
+                        min_agent_x = agent_x - 1;
+                        dist_x++;
+                    }
+                }
+                
+                agent_y = floor(xy / max_x);
+                agent_x = xy % max_x;
+                
+                north = (agent_y - 1) * max_x + agent_x;
+                east = agent_y * max_x + agent_x + 1;
+                south = (agent_y + 1) * max_x + agent_x;
+                west = agent_y * max_x + agent_x - 1;
+            }
+            
+            
+            
+            
+            // north neighbor
+            if (agent_y > 0) {
+                if (agent_pos[north] != -1) {
+                    typename EA::phenotype_type& neighbor = as[agent_pos[north]];
+                    if (sense) {
+                        (as[p]).input(0) = neighbor.output(0);
+                        (as[p]).input(1) = neighbor.output(1);
+                    }
+                    
+                    if (communication) {
+                        (as[p]).input(13) = neighbor.output(10);
+                        (as[p]).input(14) = neighbor.output(11);
+                        (as[p]).input(15) = neighbor.output(12);
+                    }
+                    
+                }
+            }
+            
+            // east neighbor
+            if (agent_x < (max_x - 2)) {
+                if (agent_pos[east] != -1) {
+                    typename EA::phenotype_type& neighbor = as[agent_pos[east]];
+                    if (sense) {
+                        (as[p]).input(2) = neighbor.output(0);
+                        (as[p]).input(3) = neighbor.output(1);
+                    }
+                    
+                    if (communication) {
+                        (as[p]).input(16) = neighbor.output(10);
+                        (as[p]).input(17) = neighbor.output(11);
+                        (as[p]).input(18) = neighbor.output(12);
+                    }
+                }
+            }
+            
+            // south neighbor
+            if (agent_y < (max_x - 2)) {
+                if (agent_pos[south] != -1) {
+                    typename EA::phenotype_type& neighbor = as[agent_pos[south]];
+                    if (sense) {
+                        (as[p]).input(4) = neighbor.output(0);
+                        (as[p]).input(5) = neighbor.output(1);
+                    }
+                    
+                    if (communication) {
+                        (as[p]).input(19) = neighbor.output(10);
+                        (as[p]).input(20) = neighbor.output(11);
+                        (as[p]).input(21) = neighbor.output(12);
+                    }
+                }
+            }
+            
+            // west neighbor
+            if (agent_x > 0) {
+                if (agent_pos[west] != -1) {
+                    typename EA::phenotype_type& neighbor = as[agent_pos[west]];
+                    if (sense) {
+                        (as[p]).input(6) = neighbor.output(0);
+                        (as[p]).input(7) = neighbor.output(1);
+                    }
+                    
+                    if (communication) {
+                        (as[p]).input(22) = neighbor.output(10);
+                        (as[p]).input(23) = neighbor.output(11);
+                        (as[p]).input(24) = neighbor.output(12);
+                    }
+                }
+            }
+            
+            
+            // origin
+            if (origin) {
+                if (agent_x == 0 and agent_y == 0) {
+                    as[p].input(8) = 1;
+                } else {
+                    as[p].input(8) = 0;
+                }
+            }
+            
+            
+            // edge
+            if (edge) {
+                if ((agent_x == 0) or (agent_y == 0) or (agent_x == (max_x -1)) or (agent_y == (max_y -1))) {
+                    as[p].input(9) = 1;
+                } else {
+                    as[p].input(9) = 0;
+                }
+            }
+            
+            if (stigmergic) {
+                as[p].input(10) = cell_color[xy][0];
+                as[p].input(11) = cell_color[xy][1];
+                as[p].input(12) = cell_color[xy][2];
+            }
+            
+            // reproduce
+            if ((total_time >= get<NO_REP_PERIOD>(ea,0)) && (reproduce && as[p].output(5))) {
+                
+                
+                if ((as[p].output(2) == 0) && (as[p].output(3)== 0) && (agent_y > 0)) { // 00 north
+                    if ((agent_pos[north] == -1) && (dist_y < dt_max_y)){
+                        as.push_back(N); // Add a new agent.
+                        agent_pos[north] = (as.size() -1); // This agent is at the end...
+                        min_agent_y = agent_y - 1;
+                        dist_y++;
+                    }
+                } else if ((as[p].output(2) == 0) && (as[p].output(3)== 1) && (agent_x < (max_x - 1))) {  // 01 east
+                    if ((agent_pos[east] == -1) && (dist_x < dt_max_x)) {
+                        as.push_back(N); // Add a new agent.
+                        agent_pos[east] = (as.size() -1); // This agent is at the end...
+                        max_agent_x = agent_x + 1;
+                        dist_x++;
+                    }
+                } else if ((as[p].output(2) == 1) && (as[p].output(3)== 1) && (agent_y < (max_y - 1))) { // 11 south
+                    if ((agent_pos[south] == -1) && (dist_y < dt_max_y)){
+                        as.push_back(N); // Add a new agent.
+                        agent_pos[south] = (as.size() -1); // This agent is at the end...
+                        max_agent_y = agent_y + 1;
+                        dist_y++;
+                    }
+                } else if ((as[p].output(2) == 1) && (as[p].output(3)== 0) && (agent_x > 0)) { // 10 west
+                    if ((agent_pos[west] == -1) && (dist_x < dt_max_x)){
+                        as.push_back(N); // Add a new agent.
+                        agent_pos[west] = (as.size() -1); // This agent is at the end...
+                        min_agent_x = agent_x - 1;
+                        dist_x++;
+                    }
+                }
+            }
+            
+            // stigmergic marking
+            if (stigmergic && as[p].output (6)) {
+                cell_color[xy][0] = as[p].output(7);
+                cell_color[xy][1] = as[p].output(8);
+                
+            }
+            
+            // apply input errors if any.
+            float error_prob = get<INPUT_BIT_ERROR_PROB>(ea,0);
+            assert(0 <= error_prob <= 1.0);
+            if (error_prob > 0) {
+                for (int k = 0; k < get<MKV_INPUT_N>(ea,0); k++) {
+                    if (ea.rng().p(error_prob)) {
+                        as[p].input(k) = ea.rng().bit();
+                    }
+                }
+                
+            }
+            
+            
+            float ep = get<GATE_EPSILON>(ea,0);
+            // update brain_updates times.
+            for (int i = 0; i<brain_updates; ++i) {
+                
+                std::size_t up = 1;
+                (as[p]).update(up,ep);
+                
+                // apply hidden errors if any.
+                float h_error_prob = get<HIDDEN_BIT_ERROR_PROB>(ea,0);
+                assert(0 <= h_error_prob <= 1.0);
+                if (h_error_prob > 0) {
+                    for (int k = 0; k < get<MKV_HIDDEN_N>(ea,0); k++) {
+                        if (ea.rng().p(h_error_prob)) {
+                            as[p].hidden(k) = ea.rng().bit();
+                        }
+                    }
+                    
+                }
+                
+                
+                // apply output errors if any.
+                float o_error_prob = get<OUTPUT_BIT_ERROR_PROB>(ea,0);
+                assert(0 <= o_error_prob <= 1.0);
+                if (o_error_prob > 0) {
+                    for (int k = 0; k < get<MKV_OUTPUT_N>(ea,0); k++) {
+                        if (ea.rng().p(o_error_prob)) {
+                            as[p].output(k) = ea.rng().bit();
+                        }
+                    }
+                    
+                }
+                
+            }
+            
+        }
+        ++total_time;
+    }
+}
+
+
+
+
+
+
+
+
+
 #endif
